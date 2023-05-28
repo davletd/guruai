50ace167529fe3e3d0def8d32e9dc3b36e59ad98
Davlet Dzhakishev <davlet@fjong.com>
2023-05-28 13:18:06 +0200

    ionic setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,28 @@
-import './App.css';
+/* Core CSS required for Ionic components to work properly */
+import '@ionic/react/css/core.css';
+
+/* Basic CSS for apps built with Ionic */
+import '@ionic/react/css/normalize.css';
+import '@ionic/react/css/structure.css'; // Remove if nothing is visible
+import '@ionic/react/css/typography.css';
+
+/* Optional CSS utils that can be commented out */
+import '@ionic/react/css/padding.css';
+import '@ionic/react/css/float-elements.css';
+import '@ionic/react/css/text-alignment.css';
+import '@ionic/react/css/text-transformation.css';
+import '@ionic/react/css/flex-utils.css';
+import '@ionic/react/css/display.css';
+import { setupIonicReact } from '@ionic/react';
+
 import { Outlet } from 'react-router-dom';
+import './App.css';
+
+setupIonicReact();
 
 function App() {
 	return (
-		<div style={{ margin: '1rem' }}>
+		<div style={{ margin: '0 1rem 0 1rem', paddingTop: 'env(safe-area-inset-top)' }}>
 			<h1>Simons App</h1>
 			<Outlet />
 		</div>
