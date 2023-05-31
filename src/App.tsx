57a11c31c6e2c825ef02e98d1545561d1c69acc3
Davlet Dzhakishev <davlet@fjong.com>
2023-05-31 18:55:16 +0200

    Deploy webapp to firebase

--- src/App.tsx
+++ src/App.tsx
@@ -14,12 +14,35 @@ import '@ionic/react/css/text-transformation.css';
 import '@ionic/react/css/flex-utils.css';
 import '@ionic/react/css/display.css';
 import { setupIonicReact } from '@ionic/react';
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
 
 import { Outlet } from 'react-router-dom';
 import { IonApp } from '@ionic/react';
 import './App.css';
 
 setupIonicReact();
+
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyACsFad0okQR335_ZIPFzcCIoAoT7vKQsM",
+  authDomain: "guruai-e5d66.firebaseapp.com",
+  projectId: "guruai-e5d66",
+  storageBucket: "guruai-e5d66.appspot.com",
+  messagingSenderId: "189040479012",
+  appId: "1:189040479012:web:29e8446e32828dc83d41b1",
+  measurementId: "G-P4XMPSNF1W"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 
 function App() {
 	return (
