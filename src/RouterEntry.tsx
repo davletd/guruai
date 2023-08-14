6405700bf5be6e6dbaf4c50c03f0cc300daf0220
Davlet Dzhakishev <davlet@fjong.com>
2023-08-14 16:12:45 +0200

    Save text to local storage

--- src/RouterEntry.tsx
+++ src/RouterEntry.tsx
@@ -1,15 +1,23 @@
 import React, { useState } from 'react';
 import App from './App';
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import { Storage } from '@ionic/storage';
+
+
 import Login from './routes/Login';
 import Dashboard from './routes/Dashboard';
 import Welcome from './routes/Welcome';
 import PageContent from './routes/PageContent';
 
+
+const storage = new Storage();
+await storage.create();
+
 const RouterEntry = () => {
 
 	const [authUser, setAuthUser] = useState({});
 	const [user, setUser] = useState({});
+
 
 	return (
 		<BrowserRouter>
@@ -18,7 +26,7 @@ const RouterEntry = () => {
 					<Route path="/" element={<Welcome />} />
 					<Route path="/Login" element={<Login setAuthUser={setAuthUser}/>} />
 					<Route path="/app/dashboard" element={<Dashboard authUser={authUser} user={user} setUser={setUser}/>} />
-					<Route path="/app/day/:dayId" element={<PageContent user={user} />} />
+					<Route path="/app/day/:dayId" element={<PageContent user={user} storage={storage} />} />
 					<Route
 						path="*"
 						element={
--- src/routes/PageContent.tsx
+++ src/routes/PageContent.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from "axios";
 import { IonContent, IonFooter, IonHeader, IonTitle, IonToolbar, IonButton } from '@ionic/react';
 import { useParams } from 'react-router-dom';
@@ -11,8 +11,37 @@ import shadowContent from '../data/content';
 const PageContent = (props: any) => {
   const { dayId } = useParams();
   const id = parseInt(dayId || "0");
-  const user = props.user;
+  const { user, storage } = props;
   const navigate = useNavigate();
+
+  const [content, setContent] = useState('');
+
+  useEffect(() => {
+      async function fetchData() {
+          try {
+              // Fetch content from the server
+              //const response = await fetch('/api/getEditorContent');
+              //const serverContent = await response.text();
+
+              const localContent = await storage.get('draftContent');
+              setContent(localContent);
+
+              // If there's content in local storage, decide which one to use
+              // if (localContent && localContent !== serverContent) {
+              //     // For simplicity, we prioritize local content here.
+              //     // Alternatively, prompt the user to decide or compare timestamps.
+              //     setContent(localContent);
+              // } else {
+              //     setContent(serverContent);
+              // }
+
+          } catch (error) {
+              console.error('Error fetching the content:', error);
+          }
+      }
+
+      fetchData();
+  }, []);
 
   const onCompleteDay = async (event: any) => {
 		event.preventDefault();
@@ -29,6 +58,11 @@ const PageContent = (props: any) => {
 		});
 	};
 
+  const onTextChange = async (newContent: any) => {
+    await storage.set('draftContent', newContent);
+    setContent(newContent);
+    //saveToServer(newContent);
+  }
 
   return (
     <>
@@ -39,7 +73,7 @@ const PageContent = (props: any) => {
       </IonHeader>
       <IonContent fullscreen={true} className="ion-padding">
         {shadowContent[id].text}
-        <ReactQuill theme="bubble" placeholder="Content goes here..."/>
+        <ReactQuill value={content} theme="bubble" placeholder="Content goes here..." onChange={onTextChange}/>
       </IonContent>
       <IonFooter>
         <IonToolbar>
